19954ef15834e6dbbfde6ef4476aef1982b0dfc0
iamxiaojin86 <iamxiaojin86@gmail.com>
2017-08-18 10:54:58 -0700

    make scull to be compile

--- scull/pipe.c
+++ scull/pipe.c
@@ -27,6 +27,7 @@
 #include <linux/poll.h>
 #include <linux/cdev.h>
 #include <asm/uaccess.h>
+#include <linux/sched.h>
 
 #include "scull.h"		/* local definitions */
 
@@ -316,7 +317,8 @@ struct file_operations scull_pipe_fops = {
 	.read =		scull_p_read,
 	.write =	scull_p_write,
 	.poll =		scull_p_poll,
-	.ioctl =	scull_ioctl,
+//	.ioctl =	scull_ioctl,
+	.unlocked_ioctl = scull_ioctl,
 	.open =		scull_p_open,
 	.release =	scull_p_release,
 	.fasync =	scull_p_fasync,
@@ -362,7 +364,8 @@ int scull_p_init(dev_t firstdev)
 	for (i = 0; i < scull_p_nr_devs; i++) {
 		init_waitqueue_head(&(scull_p_devices[i].inq));
 		init_waitqueue_head(&(scull_p_devices[i].outq));
-		init_MUTEX(&scull_p_devices[i].sem);
+//		init_MUTEX(&scull_p_devices[i].sem);
+		mutex_init(&scull_p_devices[i].sem);
 		scull_p_setup_cdev(scull_p_devices + i, i);
 	}
 #ifdef SCULL_DEBUG
